418533096f09ab5f0bc6edc2e7a7f17b5348c3a2
Cyberhoney <99543386+selmer512@users.noreply.github.com>
2026-07-10 14:39:16 -0400

    fix(greenfield): satisfy import group lint rule

--- server/src/core/http-server/api/greenfield/index.ts
+++ server/src/core/http-server/api/greenfield/index.ts
@@ -1,6 +1,7 @@
 import type { FastifyPluginAsync } from 'fastify'
 
 import type { APIOptions } from '@/core/http-server/http-server'
+
 import { postGreenfieldRequest } from './post'
 
 export const greenfieldRequestPlugin: FastifyPluginAsync<APIOptions> = async (
