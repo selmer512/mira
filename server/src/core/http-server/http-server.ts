db8037e5b79de1c739a1f7600a527da23669bf69
Cyberhoney <99543386+selmer512@users.noreply.github.com>
2026-07-10 13:55:21 -0400

    feat(greenfield): mount authenticated request route

--- server/src/core/http-server/http-server.ts
+++ server/src/core/http-server/http-server.ts
@@ -22,6 +22,7 @@ import { runActionPlugin } from '@/core/http-server/api/run-action'
 import { fetchWidgetPlugin } from '@/core/http-server/api/fetch-widget'
 import { keyMidd } from '@/core/http-server/plugins/key'
 import { utterancePlugin } from '@/core/http-server/api/utterance'
+import { greenfieldRequestPlugin } from '@/core/http-server/api/greenfield'
 import { openPathPlugin } from '@/core/http-server/api/open-path'
 import { LLM_MANAGER, PERSONA } from '@/core'
 import { SystemHelper } from '@/helpers/system-helper'
@@ -112,6 +113,9 @@ export default class HTTPServer {
       this.fastify.register((instance, _opts, next) => {
         instance.addHook('preHandler', keyMidd)
 
+        instance.register(greenfieldRequestPlugin, {
+          apiVersion: API_VERSION
+        })
         instance.register(utterancePlugin, { apiVersion: API_VERSION })
 
         // TODO: reimplement skills routes once the new core is ready
